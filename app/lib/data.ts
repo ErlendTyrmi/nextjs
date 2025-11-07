5677debcfbbf91355ff25af48df62c8286c146ad
erlendtyrmi <s132962@dtu.dk>
2025-11-07 10:41:45 +0100

    remove dev delays

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,16 +13,8 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
     console.log("Fetching revenue data...");
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
-    console.log("Data fetch completed after 3 seconds.");
-
     return data;
   } catch (error) {
     console.error("Database Error:", error);
@@ -31,7 +23,6 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
   try {
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
